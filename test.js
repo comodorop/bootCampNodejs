2dda098c4db9c170da37ad6826076eca2850e0cf
comodorop <comodoro_21@hotmail.com>
2021-10-09 15:22:51 -0500

    add utils tokens

--- test.js
+++ test.js
@@ -1,12 +1,21 @@
-const mysql = require('promise-mysql2');
+// const mysql = require('promise-mysql2');
 
-mysql.createConnection({
-    "host": "127.0.0.1",
-    "user": "roott",
-    "password": "1234565",
-    "database": "bootcamp"
-}).then(cm=>{
-    console.log("Connecction success")
-}).catch(err=>{
-    console.log("Connection DB fail")
-})
+// mysql.createConnection({
+//     "host": "127.0.0.1",
+//     "user": "roott",
+//     "password": "1234565",
+//     "database": "bootcamp"
+// }).then(cm=>{
+//     console.log("Connecction success")
+// }).catch(err=>{
+//     console.log("Connection DB fail")
+// })
+const {generateToken, validateToken} = require('./utils/token/index')
+let token = generateToken()
+
+let dataToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InVzZXIiOiJwamd0Iiwicm9sIjoiYWRtaW4ifSwiaWF0IjoxNjMzODEwODM1LCJleHAiOjE2MzM4MTA4NTB9.WBX4fHacYFUI7wOZ8WE9UOq5GrZ8CbCuVUdW1Tv8WsA'
+let data = validateToken(dataToken)
+console.log(token)
+console.log(data)
+
+
--- utils/token/index.js
+++ utils/token/index.js
@@ -1,9 +1,25 @@
-function generateToken(){
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
+function generateToken() {
+    return jwt.sign(
+        { data: { user: "pjgt", rol: "admin" }},
+        process.env.SECRET,
+        {expiresIn: 15}
+    )
+}
+
+function validateToken(token) {
+    try {
+        let data = jwt.verify(token, process.env.SECRET)
+        return data
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 
-
 module.exports = {
-    generateToken
+    generateToken,
+    validateToken
 }
