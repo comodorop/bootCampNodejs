8e1d0044cfe2dfe7b172eee646bf6544063c0b5e
comodorop <comodoro_21@hotmail.com>
2021-10-09 08:36:24 -0500

    add middleware

---
+++ middelware/index.js
@@ -0,0 +1,19 @@
+const ips = ["192.168.1.2"]
+
+module.exports = {
+    listWhiteIp: async (req, res, next) => {
+        let {body} = req
+        if (req.body.hasOwnProperty("ip")) {
+            let { ip } = req.body
+            if (ips.includes(ip)) {
+                next()
+            }
+            else {
+                return res.status(401).send({ msg: "you Dont have access" })
+            }
+        }
+        else {
+            return res.status(401).send({ msg: "Requiere ip porperty" })
+        }
+    }
+}
