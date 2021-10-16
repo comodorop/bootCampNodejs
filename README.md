093a175eba858610633e9ed9bd92ba2390868ecd
comodorop <comodoro_21@hotmail.com>
2021-10-16 08:51:32 -0500

    add readme and library to generate migrations

---
+++ README.md
@@ -0,0 +1,10 @@
+# Bootcamp
+
+To create migration to use the library local, execute the next command:
+
+To check the documentation go the next link
+https://www.npmjs.com/package/sqlmigrate
+
+```
+node_modules/.bin/sqlmigrate create --name='migration name'
+```
--- connection/mysql.connection.js
+++ connection/mysql.connection.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const mysql = require('promise-mysql2');
+
 
 let connection
 
 async function createConnection() {
     mysql.createConnection({
-        "host": "127.0.0.1",
-        "user": "root",
-        "password": "123456",
-        "database": "bootcamp"
+        "host": process.env.DB_HOST,
+        "user": process.env.DB_USER,
+        "password": process.env.DB_PASSWORD,
+        "database": process.env.DB_DATABASE
     }).then(cn => {
         connection = cn
         return cn
