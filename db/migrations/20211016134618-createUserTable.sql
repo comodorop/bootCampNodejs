03630ac563776fac0f000ed02871049071ccf956
comodorop <comodoro_21@hotmail.com>
2021-10-16 09:02:10 -0500

    add first command

--- db/migrations/20211016134618-createUserTable.sql
+++ db/migrations/20211016134618-createUserTable.sql
@@ -1,1 +1,8 @@
--- migration script --
+
+CREATE TABLE IF NOT EXISTS usersv2 (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(30) not null,
+    `created_at` DATETIME null,
+    `updated_at` DATETIME null,
+    `deleted_at` DATETIME null
+)
